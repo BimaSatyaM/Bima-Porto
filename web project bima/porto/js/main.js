b6ef94a4b09bc625c5a1b647c5bb39a9c3daaae4
Bima Satya <bimasatyamahendra07@gmail.com>
2026-05-24 15:13:09 +0700

    Delete web project bima/porto directory

--- web project bima/porto/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    initControls();
-    initProgress();
-    initAudioEvents();
-    initNavigation();
-    initSPA();
-    initWelcome();
-    console.log('%c🎵 Portfolio Ready!', 'color:#1DB954;font-size:18px;font-weight:bold;');
-});
